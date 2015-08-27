61c5e1c627d58de4dac9f0fd583c2b9a06b3e79f
Marcela Alves <marcelaffb@gmail.com>
2015-08-26 23:31:42 -0300

    Try conection

--- app/src/main/java/com/example/empowerit/RegistrationActivity.java
+++ app/src/main/java/com/example/empowerit/RegistrationActivity.java
@@ -6,17 +6,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.testing.json.MockJsonFactory;
+import com.google.api.client.util.DateTime;
+import com.smartcanvas.Smartcanvas;
+import com.smartcanvas.model.Card;
+
+import org.jose4j.lang.JoseException;
+
+import java.io.IOException;
+import java.util.Date;
+
 /**
  * Created by Marcela on 26/08/15.
  */
 public class RegistrationActivity extends Activity {
 
-   LinearLayout newRegister;
+    LinearLayout newRegister;
+    Smartcanvas smartcanvas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        newRegister = (LinearLayout)findViewById(R.id.btn_register);
+
+        try {
+
+
+            Smartcanvas smartcanvas = new Smartcanvas(new NetHttpTransport(), new MockJsonFactory(), Credential.cliente_id, Credential.access_token);
+            Card card = new Card(givenProvider());
+            String s = card.getSummary();
+            Card.Author author = new Card.Author();
+            author.setDisplayName("Marcela Alves");
+            author.setImageURL("http://www.pubzi.com/f/731147-sm-731131-Cartoon-Flower.png");
+           card.setAuthor(author);
+            Date date = new Date(); // java.util.Date; - This date has both the date and time in it already.
+            DateTime dateTime = new DateTime(date);
+            card.setCreateDate(dateTime);
+            card.setId(Long.valueOf(1234));
+            card.setContent("App Company");
+            card.setTitle("EmpowerIT");
+            smartcanvas.cards().insert(card);
+            Card newCard = smartcanvas.cards().get(Long.valueOf(1234));
+
+
+        } catch (JoseException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        newRegister = (LinearLayout) findViewById(R.id.btn_register);
         newRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +71,10 @@ public class RegistrationActivity extends Activity {
         });
 
     }
+
+    private Card.ContentProvider givenProvider() {
+        return new Card.ContentProvider("ID provider", "GooglePlus", "Content ID");
+    }
+
 
 }
